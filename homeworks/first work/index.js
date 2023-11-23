245d23366e2d650e25c50280adcf4acf9d120feb
“KateAkulenko” <katyanasoloda@gmail.com>
2023-11-24 00:39:13 +0200

    first homework

---
+++ homeworks/first work/index.js
@@ -0,0 +1,27 @@
+// let _name = "";
+// let second_name = "";
+// let $name = "";
+// let secondName = "";
+
+let $name = prompt('Як вас звати');
+alert (`Привіт ${$name}`);
+
+let age = prompt('вкажіть рік вашого народження');
+const year = 2023;
+alert (`Ваш вік ${year-age}`);
+
+let sideofasquare = prompt ('вкажіть сторону квадрату в сантиметрах');
+alert (`Периметр ${sideofasquare*4} см`);
+
+let radius = prompt ('вкажіть радіус кола у сантиметрах');
+const pi = 3.14;
+alert (`Площа кола ${pi*(radius*radius)}`);
+
+let distance = prompt ('Вкажіть відстань між містами в кілометрах');
+let time = prompt ('Вкажіть час за який ви хочете доїхати в годинах');
+alert (`Необхідна швидкість ${distance/time} км/год`);
+
+let dollar = prompt ('Скільки Долярів маш на кармані?');
+const kurs = 0.92;
+alert (`${dollar*kurs} Євро`);
+
